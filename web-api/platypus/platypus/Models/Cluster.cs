f8c4f9d550abfb021fdd609dca07e2c113027287
Reiw98 <Reiw98@users.noreply.github.com>
2019-11-13 13:49:29 +0900

    クラスタ管理用モデルを追加

---
+++ web-api/platypus/platypus/Models/Cluster.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nssol.Platypus.Models
+{
+    /// <summary>
+    /// クラスタ
+    /// </summary>
+    public class Cluster : ModelBase
+    {
+        /// <summary>
+        /// クラスタホスト名
+        /// </summary>
+        [Required]
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// 表示名
+        /// </summary>
+        [Required]
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// リソース管理キー。
+        /// クラスタのトークン。
+        /// </summary>
+        [Required]
+        public string ResourceManageKey { get; set; }
+
+        /// <summary>
+        /// メモ
+        /// </summary>
+        public string Memo { get; set; }
+    }
+}
---
+++ web-api/platypus/platypus/Models/ClusterTenantMap.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nssol.Platypus.Models
+{
+    /// <summary>
+    /// クラスタとテナントの対応付け中間テーブル。
+    /// <seealso cref="Cluster"/>
+    /// </summary>
+    public class ClusterTenantMap : ModelBase
+    {
+        /// <summary>
+        /// クラスタID
+        /// </summary>
+        [Required]
+        public long ClusterId { get; set; }
+
+        /// <summary>
+        /// テナントID
+        /// </summary>
+        [Required]
+        public long TenantId { get; set; }
+
+        /// <summary>
+        /// クラスタ
+        /// </summary>
+        [ForeignKey(nameof(ClusterId))]
+        public virtual Cluster Cluster { get; set; }
+
+        /// <summary>
+        /// テナント
+        /// </summary>
+        [ForeignKey(nameof(TenantId))]
+        public virtual Tenant Tenant { get; set; }
+    }
+}
